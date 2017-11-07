f7126847d9aacdc7a50811a909caa9dfb4ef29ef
Shishkov Ivan <cellentano123.1999@gmail.com>
2017-11-07 15:55:05 +0300

    Add files via upload

---
+++ SoketServer.py
@@ -0,0 +1,9 @@
+import socket
+
+s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+s.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
+s.bind(('0.0.0.0',11719))
+while  1:
+	message = s.recv(128)
+	print (message)
